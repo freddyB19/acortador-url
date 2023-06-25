16bba4822e269e7c6f762069569c1eb232baac64
freddyB19 <freddyybolivar2013@gmail.com>
2023-06-25 15:31:52 -0400

    configuración del archivo build, segunda parte

--- build.sh
+++ build.sh
@@ -5,4 +5,6 @@ set -o errexit
 pip install -r requeriments.txt
 
 python manage.py collectstatic --no-input
+python manage.py makemigrations 
+python manage.py migrate auth
 python manage.py migrate --run-syncdb
